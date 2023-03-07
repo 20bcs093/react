96782f75e612aa72a9bab7aed1abcf92e45b4d81
MAHA DHANUSH <100668434+20bcs093@users.noreply.github.com>
2023-03-07 22:14:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const router=require('./routes/routes')
+const app=express()
+const cors=require('cors')
+require('./models/db')
+app.use(express.json())
+app.use(cors())
+app.use('/api/tasks',router)
+app.listen('8000',err=>{
+    if(err) 
+    console.log(err)
+    console.log('server is started at port number:8000')
+})
